cd83db80419fc706e23e4d6651a12832919e93b6
Rodell <rodriguez.rodell53@gmail.com>
2020-12-06 18:32:51 -0500

    starting return kth to last

---
+++ linked_lists/linked_list.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data): # inserts new node at the end of the LL
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            # never manipulate self.head directly since we always want access to the head of the LL
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+
+    def print(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def delete(self, data):
+        prev = None
+        current = self.head
+        while current:
+            if current.data == data:
+                if prev:
+                    prev.next = current.next
+                else:
+                    # for when we need to delete the head
+                    self.head = self.head.next
+                return
+            prev = current
+            current = current.next
+        raise LookupError('{} is not in the linked list'.format(data))
+
+    def get_as_a_list(self):
+        new_list = []
+        current_node = self.head
+        while current_node:
+            new_list.append(current_node.data)
+            current_node = current_node.next
+        return new_list
---
+++ linked_lists/return_kth_to_last.py
@@ -0,0 +1,8 @@
+'''
+Implement an algorithm to find the kth to last element of a singly linked list.
+'''
+'''
+
+'''
+
+from linked_list import LinkedList
