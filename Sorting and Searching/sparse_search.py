1890ad3b25a5705c46143e77de8dd7eab58d87ce
Rodell <rodriguez.rodell53@gmail.com>
2021-03-23 23:39:33 -0400

    starting over. Ran into infinite loop

--- Sorting and Searching/sparse_search.py
+++ Sorting and Searching/sparse_search.py
@@ -9,42 +9,14 @@ Output:4
 Searching for a string in a sorted string typically implies a binary search.
 Do a binary search and if it's an empty string then search for the nearest non empty string
 
-What do we mean by nearest? Do we go forward or backwards?
-
-
-1. have start, end, mid indices.
-2. While start <= end:
-    - mid = (start)//end
-    - if len(array[mid]) == 0:
-        current = mid - 1
-        while current >= start:
-            if len(array[current]) == 0:
-                current -= 1
-                continue
-            else: we did encounter a value
-                if array[current] < input:
-                    start = mid +1
-                elif array[current] > input:
-                    end = current - 1
-                else:
-                    return current
-                break
-        if current < start:
-            start = mid + 1
-    - else:
-        if array[mid] < input:
-            start = mid +1
-        elif array[mid] > input:
-            end = mid - 1
-        else:
-            return mid
+What do we mean by nearest? Do we go forward or backwards? What if we do both?
 '''
 
 
 def sparse_search(array, target):
-    pass
+
 
 
 array = ["at", "", '"', "", "ball", "", "", "car", "", '"', "dad", '"', ""]
-target = 'at'
+target = 'dad'
 assert sparse_search(array, target) == 0
